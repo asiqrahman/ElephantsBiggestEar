6fc6d3052ced11222ce8c5092064fc3c9209719a
asiq rahman <aasiqrrahman@yahoo.com>
2022-11-05 00:37:14 +0600

    Add project files.

---
+++ ElephantsBiggestEar/Elephant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElephantsBiggestEars
+{
+    class Elephant
+    {
+        public int EarSize;
+        public string Name;
+        public void WhoAmI()
+        {
+            Console.WriteLine("I am " + Name + " my ear size is " + EarSize + ".");
+        }
+        public void HearMessage(string message, Elephant WhoSaidIt)
+        {
+            Console.WriteLine(Name + " heard a message");
+            Console.WriteLine(WhoSaidIt.Name + " said this: " + message);
+        }
+        public void SpeakTo(Elephant WhoToTalkTo, string message)
+        {
+            WhoToTalkTo.HearMessage(message,this);
+        }
+    }
+}
---
+++ ElephantsBiggestEar/Program.cs
@@ -0,0 +1,30 @@
+﻿using ElephantsBiggestEars;
+using System;
+namespace ElephantsBiggeastEar
+{
+    class program
+    {
+        private static void Main(string[] args)
+        {
+            Elephant[] elephants = new Elephant[7];
+            elephants[0] = new Elephant() { Name = "Lloyd", EarSize = 40 };
+            elephants[1] = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            elephants[2] = new Elephant() { Name = "Larry", EarSize = 42 };
+            elephants[3] = new Elephant() { Name = "Lucille", EarSize = 32 };
+            elephants[4] = new Elephant() { Name = "Lars", EarSize = 44 };
+            elephants[5] = new Elephant() { Name = "Linda", EarSize = 37 };
+            elephants[6] = new Elephant() { Name = "Humphery", EarSize = 45 };
+
+            Elephant biggestear = elephants[0];
+            for(int i = 1; i < elephants.Length; i++)
+            {
+                Console.WriteLine("iteration #" + i);
+                if (elephants[i].EarSize > biggestear.EarSize)
+                {
+                    biggestear = elephants[i];
+                }
+                Console.WriteLine(biggestear.EarSize.ToString());
+            }
+        }
+    }
+}
